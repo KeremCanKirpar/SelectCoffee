b84a08b694c2ac307c94b7f99a572e258ac1aa28
Maverick <89163711+Maverivck@users.noreply.github.com>
2024-07-20 13:52:31 +0300

    Add project files.

---
+++ Kahve/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Kahve.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:CheckBox ID="CheckBox1" runat="server" Text="Süt" />
+            <br />
+            <asp:CheckBox ID="CheckBox2" runat="server" Text="Krema" />
+            <br />
+            <asp:CheckBox ID="CheckBox3" runat="server" Text="Ekstra Kahve" />
+            <br />
+            <asp:CheckBox ID="CheckBox4" runat="server" Text="Şeker" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="ekle" Width="91px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Kahve/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kahve
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+         String secim ;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(CheckBox1.Checked)
+            {
+                secim += CheckBox1.Text + ",";
+            }
+            if (CheckBox2.Checked)
+            {
+                secim += CheckBox2.Text + ",";
+            }
+            if (CheckBox3.Checked)
+            {
+                secim += CheckBox3.Text + ",";
+            }
+            if (CheckBox4.Checked)
+            {
+                secim += CheckBox4.Text + ",";
+            }
+            Label1.Visible = true;
+            Label1.Text = "Kahvenize " + secim + " Eklendi";
+        }
+    }
+}
